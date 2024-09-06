7bee7e844ef9f0552991df043d3c1bcd42554fa2
Nguinabe3 <nguinabe.josue@gmail.com>
2024-09-06 15:29:28 +0000

    adding tranLR.py file

--- SimpleML.py
+++ SimpleML.py
@@ -1,4 +1,7 @@
 from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score
 import pandas as pd
 
 def load_dataset():
@@ -16,5 +19,25 @@ def load_dataset():
     df['target_names'] = df['target'].map(target_names)
     print(df.head())
     return df
+
+def train(df):
+    X_train, X_test, y_train, y_test = train_test_split(
+        df.iloc[:, :-1], df["target"], test_size=0.2, random_state=42
+    )
+
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
+
+    return model, X_train, X_test, y_train, y_test
+
+def get_accuracy(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    accuracy = accuracy_score(y_test, predictions)
+
+    return accuracy
+
 if __name__ == "__main__":
-    load_dataset()
+    df=load_dataset()
+    model, X_train, X_test, y_train, y_test = train(df)
+    accuracy = get_accuracy(model, X_test, y_test)
+    print(f"Accuracy: {accuracy:.2f}")
