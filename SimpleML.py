1e349e322df7a8e5c8e31630f1b065bce2e4bbbe
Nguinabe3 <nguinabe.josue@gmail.com>
2024-09-07 09:59:46 +0000

    Adding extra functions

--- SimpleML.py
+++ SimpleML.py
@@ -3,6 +3,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import ConfusionMatrixDisplay, accuracy_score
 import pandas as pd
+import matplotlib.pyplot as plt
 
 def load_dataset():
     # Load the Iris dataset
@@ -36,8 +37,44 @@ def get_accuracy(model, X_test, y_test):
 
     return accuracy
 
+def plot_feature(df, feature):
+    # Plot a histogram of one of the features
+    df[feature].hist()
+    plt.title(f"Distribution of {feature}")
+    plt.xlabel(feature)
+    plt.ylabel("Frequency")
+    plt.show()
+
+
+def plot_features(df):
+    # Plot scatter plot of first two features.
+    scatter = plt.scatter(
+        df["sepal length (cm)"], df["sepal width (cm)"], c=df["target"]
+    )
+    plt.title("Scatter plot of the sepal features (width vs length)")
+    plt.xlabel(xlabel="sepal length (cm)")
+    plt.ylabel(ylabel="sepal width (cm)")
+    plt.legend(
+        scatter.legend_elements()[0],
+        df["target_names"].unique(),
+        loc="lower right",
+        title="Classes",
+    )
+    plt.show()
+
+
+def plot_model(model, X_test, y_test):
+    # Plot the confusion matrix for the model
+    ConfusionMatrixDisplay.from_estimator(estimator=model, X=X_test, y=y_test)
+    plt.title("Confusion Matrix")
+    plt.show()
+
 if __name__ == "__main__":
     df=load_dataset()
     model, X_train, X_test, y_train, y_test = train(df)
     accuracy = get_accuracy(model, X_test, y_test)
     print(f"Accuracy: {accuracy:.2f}")
+
+    plot_feature(df, "sepal length (cm)")
+    plot_features(df)
+    plot_model(model, X_test, y_test)
